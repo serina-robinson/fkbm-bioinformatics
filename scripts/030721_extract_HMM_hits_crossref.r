0b001b5e99161e5a3771cc0cb21cc7124d6b0465
Serina Robinson <robinsonserinalee@gmail.com>
2021-07-08 22:40:37 +0200

    aesthetic changes to phylogeny figure

--- scripts/030721_extract_HMM_hits_crossref.r
+++ scripts/030721_extract_HMM_hits_crossref.r
@@ -4,26 +4,23 @@ pacman::p_load("tidyverse", "Biostrings",
 
 # Read in HMM hits
 hits <- read_delim("data/HMMsearch_NCcutoff_40MTs.txt", delim = " ", col_names = F)
+# parsing failure final column ok
 hits$X9 <- trimws(hits$X9)
 accs <- word(hits$X9, sep = "\\|", -1) # 
-accs
 
 # Read in the MIBiG sequences
 mibig <- readAAStringSet("data/mibig_prot_seqs_2.0.fasta")
 inds <- grep(paste0(accs, collapse = "|"), names(mibig))
 met_hits <- mibig[inds]
-names(met_hits)
-names(met_hits)
 
-# Also cross-reference with organism and sequence type
+# Cross-reference with organism and sequence type
 metadat <- read_excel("data/bigfam_results.xlsx") %>%
-  dplyr::select(-genome) 
-metadat$BGC <- word(metadat$BGC, sep = "\\.gbk", 1)
-metadat$taxon <- gsub("\\(Species\\)\\?", "", metadat$taxon)
-metadat$taxon <- gsub("\\(Organism\\)\\?", "", metadat$taxon)
+  dplyr::select(-genome) %>%
+  dplyr::mutate(BGC = word(BGC, sep = "\\.gbk", 1)) %>%
+  dplyr::mutate(taxon = gsub("\\(Species\\)\\?", "", taxon)) %>%
+  dplyr::mutate(taxon = gsub("\\(Organism\\)\\?", "", taxon))
 
 # Make unique tree identifiers
-names(met_hits)
 metdf <- data.frame(bgc = word(names(met_hits), sep = "\\|", 1),
                     nams = names(met_hits),
                     aa = met_hits) %>%
@@ -35,9 +32,6 @@ manual <- read_excel("data/39_MIBiG_Methyltransf_21_lit_dat_manually_curated.xls
   dplyr::filter(include_in_tree == "yes") %>%
   dplyr::filter(!duplicated(acc))
 
-metdf[!metdf$bgc %in% manual$bgc,]
-metdf$bgc %in% manual$bgc
-
 merg <- manual %>%
   inner_join(., metdf, keep = F, by = "bgc", suffix = c("", ".y")) %>%
   select_at(
@@ -46,39 +40,25 @@ merg <- manual %>%
   dplyr::group_by(acc) %>%
   dplyr::slice(1) %>%
   ungroup()
-merg
 
-write_csv(merg, "data/29_MIBiG_hits_manually_curated_to_include.csv")
-# Manually identified and replaced the missing taxon
-# https://mibig.secondarymetabolites.org/repository/BGC0000769/index.html#r1c1
-# BGC0000769
-# Mycobacterium avium subsp. hominissuis A5
-# Saccharide
-# metdf$taxon[is.na(metdf$taxon)] <- "Mycobacterium avium subsp. hominissuis A5"           
-# metdf$class[is.na(metdf$class)] <- "Saccharide: unknown" 
-# metdf$dataset[is.na(metdf$dataset)] <- "mibig"
-# write_csv(metdf, "data/39_MIBiG_Methyltransf_21_lit_dat.csv")
-# Make a new AA set with updated names
+# write_csv(merg, "data/29_MIBiG_hits_manually_curated_to_include.csv")
+
+# Create AAStringSet
 aaset <- AAStringSet(merg$aa)
-
-# names(aaset) <- paste(merg$bgc, merg$acc, merg$taxon, merg$natural_product, merg$class, sep = "_")
 names(aaset) <- paste(merg$acc, merg$taxon, merg$natural_product, merg$class, sep = "_")
-names(aaset)
-
 names(aaset) <- gsub(" |-|,|\\.", "_", names(aaset))
 names(aaset) <- gsub(":unknown", "", names(aaset))
 names(aaset) <- gsub("__|___", "_", names(aaset))
 names(aaset) <- gsub("NRP_Polyketide", "NRPPolyketide", names(aaset))
 
-# Now add in the Eremiobacterota sequence
+# Add in the Eremiobacterota N-methyltransferase EreM sequence
 erem <- readAAStringSet("data/Eudoremicrobiaceae_EreM.fasta.txt")
-names(erem)
-poyE <- readAAStringSet("data/PoyE_AerE_renamed.fasta")
 
+# Add outgroups
+poyE <- readAAStringSet("data/PoyE_AerE_renamed.fasta")
 aacomb <- AAStringSet(c(poyE, erem, aaset))
 length(aacomb)
-names(aacomb)
-writeXStringSet(aacomb, "data/32_MT_homologs_with_outgroup.fasta")
+# writeXStringSet(aacomb, "data/32_MT_homologs_with_outgroup.fasta")
 
 # Align using muscle
 # muscle -in 32_MT_homologs_with_outgroup.fasta -out 32_MT_homologs_with_outgroup_aligned.afa
@@ -89,7 +69,7 @@ writeXStringSet(aacomb, "data/32_MT_homologs_with_outgroup.fasta")
 
 hmmal <- readAAStringSet("data/32_MT_homologs_PoyE_outgroup_aligned_50gaps_removed_muscle.afa")
 hmmal <- AAStringSet(toupper(hmmal))
-BrowseSeqs(hmmal, "32_MTs_homologs_PoyE_outgroup_aligned_50gaps_removed.html")
+# BrowseSeqs(hmmal, "32_MTs_homologs_PoyE_outgroup_aligned_50gaps_removed.html")
 
 mthomologs <- readAAStringSet("data/32_MT_homologs_PoyE_outgroup_aligned_50gaps_removed_muscle.afa")
 names(mthomologs) <- gsub(" 347 bp", "", names(mthomologs))
@@ -97,7 +77,7 @@ BrowseSeqs(mthomologs)
 writeXStringSet(mthomologs, "data/32_MT_homologs_PoyE_outgroup_aligned_50gaps_removed_muscle.afa")
 names(mthomologs)[1]
 
-# Tree properly using Iqtree
+# Tree with IQ-TREE 2 Model selection:
 # iqtree -s HMMaligned_trimmed_40_MTs.fasta -m MFP 
 # Akaike Information Criterion:           VT+F+R6
 # Corrected Akaike Information Criterion: VT+F+R5
@@ -116,7 +96,7 @@ names(mthomologs)[1]
 # Likelihood distances:          HMMaligned_trimmed_40_MTs_for_bootstrap.fasta.mldist
 # 
 # Ultrafast bootstrap approximation results written to:
-#   Split support values:          HMMaligned_trimmed_40_MTs_for_bootstrap.fasta.splits.nex
+# Split support values:          HMMaligned_trimmed_40_MTs_for_bootstrap.fasta.splits.nex
 # Consensus tree:                HMMaligned_trimmed_40_MTs_for_bootstrap.fasta.contree
 # Screen log file:               HMMaligned_trimmed_40_MTs_for_bootstrap.fasta.log
 
--- scripts/030721_phylogeny.r
+++ scripts/030721_phylogeny.r
@@ -3,27 +3,22 @@ pacman::p_load("ggtree", "stringr", "ggrepel", "RColorBrewer", "scales")
 
 # Read in the consensus tree
 tr <- read.tree("data/32_MT_homologs_PoyE_outgroup_aligned_50gaps_removed_muscle.afa.contree")
-tr$tip.label[grep("study", tr$tip.label)]
-# Fix some tip labels that are having problems
-# tr$tip.label[tr$tip.label == "BGC0000769_AAN05766_1_NA_NA"] <- "BGC0000769_AAN05766_1_Mycobacterium_avium_subsp_hominis_A5_Saccharide"
+
+# Fix  formatting of tip labels
 tr$tip.label <- gsub("NRP_Polyketide", "NRPPolyketide", tr$tip.label)
-tr$tip.label <- gsub("_Saccharide", "_Polyketide", tr$tip.label) # also polyketide, removing saccharide 
+tr$tip.label <- gsub("_Saccharide", "_Polyketide", tr$tip.label) # also a polyketide, removing saccharide column
 tr$tip.label
+
 # Ggtree
 gtr <- ggtree(tr, layout = "rectangular", 
               color = "gray50", size = 0.5)
-gtr$data$label
 
 pal <- c("#BBD374", # NRPS
          "#F0D468", # Other
-         "gray80", #
-        # "#909EC4",  #T2/T3 PKS 
          "#CF8BBA", # T1 PKS
          "#EC8F6B") #RiPPs
-       #  "#8FBDA7") # Terpene
 
 metadat <- data.frame(label = gtr$data$label[gtr$data$isTip]) %>%
- # dplyr::mutate(acc = paste0(word(label, sep = "_", 1), "_", word(label, sep = "_", 2))) %>%
   dplyr::mutate(bgc_type = paste0(word(label, sep = "_", -1))) %>%
   dplyr::mutate(bgc_color1 = case_when(grepl("Polyketide", label) ~ "#CF8BBA",
                                       grepl("Other", label) ~ "#F0D468",
@@ -41,7 +36,8 @@ metadat <- data.frame(label = gtr$data$label[gtr$data$isTip]) %>%
   dplyr::mutate(label_trim = gsub("Sare ", "Sare_", label_trim)) %>%
     dplyr::mutate(label_trim = gsub("A 201A", "A-201A", label_trim)) %>%
     dplyr::mutate(label_trim = gsub("A.102395", "A-102395", label_trim)) %>%
-  dplyr::mutate(label_trim = gsub("l 2 Amino 4 methoxy trans 3 butenoic acid", "l-2-Amino-4-methoxy-trans-3-butenoic acid", label_trim)) %>%
+  dplyr::mutate(label_trim = gsub("l 2 Amino 4 methoxy trans 3 butenoic acid",
+                                  "l-2-Amino-4-methoxy-trans-3-butenoic acid", label_trim)) %>%
   dplyr::mutate(O_vs_N = case_when(grepl("study", label) ~ "N",
                                    grepl("polytheonamide|aeronamide", label) ~ NA_character_,
                 TRUE ~ "O")) %>%
@@ -52,23 +48,14 @@ metadat <- data.frame(label = gtr$data$label[gtr$data$isTip]) %>%
   dplyr::mutate(tiplab_color = case_when(grepl("study", label) ~  "#EC8F6B",
                                          grepl("polytheonamide|aeronamide", label) ~ "gray60",
                                          TRUE ~ "black")) 
-
-
-metadat$label_trim
-
 gdf <- gtr %<+% metadat
 
-
-
-pdf("output/40_MT_tree_v7.pdf", width = 12, height = 7)
+pdf("output/20210704_40_MT_tree.pdf", width = 12, height = 7)
 gdf +
   geom_nodelab(aes(label = label), size = 3) + 
-  #size = ifelse(as.numeric(gdf$data$label[!gdf$data$isTip]) > 75, 8, 0.0001),
-  #  color = ifelse(as.numeric(gdf$data$label[!gdf$data$isTip]) > 75, "gray50", NA)) +
   geom_tippoint(x = 15.8, color = gdf$data$bgc_color1[gdf$data$isTip], size = 6) +
   geom_tippoint(x = 16.2, color = ifelse(!is.na(gdf$data$bgc_color2[gdf$data$isTip]), "#BBD374", "white"), size = 6) +
   geom_tiplab(aes(label = label_trim), color = gdf$data$tiplab_color[gdf$data$isTip],
-              #size = ifelse(grepl("study", gdf$data$label_trim[gdf$data$isTip]), 8, 6),
               fontface = ifelse(grepl("study", gdf$data$label_trim[gdf$data$isTip]), "bold.italic", "italic"), align=T) + 
   geom_label2(x = 15.4, aes(subset = isTip), fill = gdf$data$O_vs_N_color[gdf$data$isTip], #alpha = 0.5,
              color = "black", label = gdf$data$O_vs_N[gdf$data$isTip])  +
@@ -76,11 +63,9 @@ gdf +
 dev.off()
 
 
-# Make a legend
-
+# Plot legend
 pal <- c("#BBD374", # NRPS
          "#F0D468", # Other #
-         # "#909EC4",  #T2/T3 PKS 
          "#CF8BBA", # T1 PKS
          "#EC8F6B") # RiPP
 txt <- c("NRPS", "Other",
@@ -92,70 +77,3 @@ legend("topleft", legend=txt, fill=pal,
        border=FALSE, bty="n", title = "BGC type")
 dev.off()
 
-
-### Try daylight tree layout
-# Ggtree
-# gtr <- ggtree(tr, layout = "daylight", 
-#               color = "gray50", size = 0.5)
-# gtr$data$label
-# 
-# pal <- c("#BBD374", # NRPS
-#          "#F0D468", # Other
-#          "gray80", #
-#          # "#909EC4",  #T2/T3 PKS 
-#          "#CF8BBA", # T1 PKS
-#          "#EC8F6B") #RiPPs
-# #  "#8FBDA7") # Terpene
-# 
-# metadat <- data.frame(label = gtr$data$label[gtr$data$isTip]) %>%
-#   # dplyr::mutate(acc = paste0(word(label, sep = "_", 1), "_", word(label, sep = "_", 2))) %>%
-#   dplyr::mutate(bgc_type = paste0(word(label, sep = "_", -1))) %>%
-#   dplyr::mutate(bgc_color1 = case_when(grepl("Polyketide", label) ~ "#CF8BBA",
-#                                        grepl("Other", label) ~ "#F0D468",
-#                                        grepl("NRP", label) ~ "#BBD374",
-#                                        grepl("study", label) ~ "#EC8F6B",
-#                                        grepl("polytheonamide|aeronamide", label) ~ NA_character_)) %>%
-#   dplyr::mutate(bgc_color2 = case_when(grepl("NRPPolyketide", label) ~ "#BBD374")) %>%
-#   dplyr::mutate(label_trim = gsub("_Polyketide|_NRPPolyketide|_Other|_NRP", "", label)) %>%
-#   dplyr::mutate(label_trim = gsub("_1", ".1", label_trim)) %>%
-#   dplyr::mutate(label_trim = gsub("_", " ", label_trim)) %>%
-#   dplyr::mutate(label_trim = gsub("WP ", "WP_", label_trim)) %>%
-#   dplyr::mutate(label_trim = gsub("BCRE ", "BCRE_", label_trim)) %>%
-#   dplyr::mutate(label_trim = gsub("STRAU ", "STRAU_", label_trim)) %>%
-#   dplyr::mutate(label_trim = gsub("AMYAL ", "AMYAL_", label_trim)) %>%
-#   dplyr::mutate(label_trim = gsub("Sare ", "Sare_", label_trim)) %>%
-#   dplyr::mutate(label_trim = gsub("A 201A ", "A-201A", label_trim)) %>%
-#   dplyr::mutate(label_trim = gsub("A.102395 ", "A-102395", label_trim)) %>%
-#   dplyr::mutate(label_trim = gsub("l 2 Amino 4 methoxy trans 3 butenoic acid", "l-2-Amino-4-methoxy-trans-3-butenoic acid", label_trim)) %>%
-#   dplyr::mutate(O_vs_N = case_when(grepl("study", label) ~ "N",
-#                                    grepl("polytheonamide|aeronamide", label) ~ NA_character_,
-#                                    TRUE ~ "O")) %>%
-#   dplyr::mutate(O_vs_N_color = case_when(grepl("AMYAL_RS47350|AAK83186|BAJ52700|PA2302", label) ~ bgc_color1,
-#                                          grepl("study", label) ~ bgc_color1,
-#                                          grepl("polytheonamide|aeronamide", label) ~ NA_character_,
-#                                          TRUE ~ "gray90")) %>%
-#   dplyr::mutate(tiplab_color = case_when(grepl("study", label) ~  "#EC8F6B",
-#                                          grepl("polytheonamide|aeronamide", label) ~ "gray60",
-#                                          TRUE ~ "black")) 
-# 
-# 
-# metadat$label_trim
-# 
-# gdf <- gtr %<+% metadat
-# 
-# 
-# 
-# pdf("output/40_MT_tree_daylight.pdf", width = 8, height = 8)
-# gdf +
-#   geom_nodelab(aes(label = label), size = 3)  +
-#   #size = ifelse(as.numeric(gdf$data$label[!gdf$data$isTip]) > 75, 8, 0.0001),
-#   #  color = ifelse(as.numeric(gdf$data$label[!gdf$data$isTip]) > 75, "gray50", NA)) +
-#   geom_tippoint(color = gdf$data$bgc_color1[gdf$data$isTip], size = 6) 
-#  # geom_tippoint(color = ifelse(!is.na(gdf$data$bgc_color2[gdf$data$isTip]), "#BBD374", "white"), size = 6)  
-#   #geom_tiplab(aes(label = label_trim), color = gdf$data$tiplab_color[gdf$data$isTip],
-#               #size = ifelse(grepl("study", gdf$data$label_trim[gdf$data$isTip]), 8, 6),
-#  #              fontface = ifelse(grepl("study", gdf$data$label_trim[gdf$data$isTip]), "bold.italic", "italic"), align=T) + 
-#   # geom_label2(x = 15.4, aes(subset = isTip), fill = gdf$data$O_vs_N_color[gdf$data$isTip], alpha = 0.5,
-#   #             color = "gray40", label = gdf$data$O_vs_N[gdf$data$isTip])  +
-#  # xlim_tree(5)
-# dev.off()
